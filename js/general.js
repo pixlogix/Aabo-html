c6d56cd67e68ac9dc7e0487f15c61d703b85a002
Pixlogix <pm@pixlogix.com>
2022-11-18 12:28:07 +0530

    First Commit
    First Commit

---
+++ js/general.js
@@ -0,0 +1,186 @@
+/* Custom General jQuery
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+;(function($, window, document, undefined) {
+	//Genaral Global variables
+	//"use strict";
+	var $win = $(window);
+	var $doc = $(document);
+	var $winW = function(){ return $(window).width(); };
+	var $winH = function(){ return $(window).height(); };
+	var $screensize = function(element){  
+			$(element).width($winW()).height($winH());
+		};
+		
+		var screencheck = function(mediasize){
+			if (typeof window.matchMedia !== "undefined"){
+				var screensize = window.matchMedia("(max-width:"+ mediasize+"px)");
+				if( screensize.matches ) {
+					return true;
+				}else {
+					return false;
+				}
+			} else { // for IE9 and lower browser
+				if( $winW() <=  mediasize ) {
+					return true;
+				}else {
+					return false;
+				}
+			}
+		};
+
+	$doc.ready(function() {
+/*--------------------------------------------------------------------------------------------------------------------------------------*/		
+		// Remove No-js Class
+		$("html").removeClass('no-js').addClass('js');
+		
+		
+		
+		/* Get Screen size
+		---------------------------------------------------------------------*/
+		$win.on('load', function(){
+			$win.on('resize', function(){
+				$screensize('your selector');	
+			}).resize();	
+		});
+		
+		
+		/* Menu ICon Append prepend for responsive
+		---------------------------------------------------------------------*/
+		$(window).on('resize', function(){
+			if (screencheck(767)) {
+				if(!$('#menu').length){
+					$('#mainmenu').prepend('<a href="#" id="menu" class="menulines-button"><span class="menulines"></span> <em>Menu</em></a>');
+				}
+			} else {
+				$("#menu").remove();
+			}
+		}).resize();
+
+		
+		/* Tab Content box 
+		---------------------------------------------------------------------*/
+		var tabBlockElement = $('.tab-data');
+			$(tabBlockElement).each(function() {
+				var $this = $(this),
+					tabTrigger = $this.find(".tabnav li"),
+					tabContent = $this.find(".tabcontent");
+					var textval = [];
+					tabTrigger.each(function() {
+						textval.push( $(this).text() );
+					});	
+				$this.find(tabTrigger).first().addClass("active");
+				$this.find(tabContent).first().show();
+
+				$(tabTrigger).on('click',function () {
+					$(tabTrigger).removeClass("active");
+					$(this).addClass("active");
+					$(tabContent).hide().removeClass('visible');
+					var activeTab = $(this).find("a").attr("data-rel");
+					$this.find('#' + activeTab).fadeIn('normal').addClass('visible');
+								
+					return false;
+				});
+			
+				var responsivetabActive =  function(){
+				if (screencheck(767)){
+					if( !$this.find('.tabMobiletrigger').length ){
+						$(tabContent).each(function(index) {
+							$(this).before("<h2 class='tabMobiletrigger'>"+textval[index]+"</h2>");	
+							$this.find('.tabMobiletrigger:first').addClass("rotate");
+						});
+						$('.tabMobiletrigger').click('click', function(){
+							var tabAcoordianData = $(this).next('.tabcontent');
+							if($(tabAcoordianData).is(':visible') ){
+								$(this).removeClass('rotate');
+								$(tabAcoordianData).slideUp('normal');
+								//return false;
+							} else {
+								$this.find('.tabMobiletrigger').removeClass('rotate');
+								$(tabContent).slideUp('normal');
+								$(this).addClass('rotate');
+								$(tabAcoordianData).not(':animated').slideToggle('normal');
+							}
+							return false;
+						});
+					}
+						
+				} else {
+					if( $('.tabMobiletrigger').length ){
+						$('.tabMobiletrigger').remove();
+						tabTrigger.removeClass("active");
+						$this.find(tabTrigger).removeClass("active").first().addClass('active');
+						$this.find(tabContent).hide().first().show();				
+					}		
+				}
+			};
+			$(window).on('resize', function(){
+				if(!$this.hasClass('only-tab')){
+					responsivetabActive();
+				}
+			}).resize();
+		});
+		
+		/* Accordion box JS
+		---------------------------------------------------------------------*/
+		$('.accordion-databox').each(function() {
+			var $accordion = $(this),
+				$accordionTrigger = $accordion.find('.accordion-trigger'),
+				$accordionDatabox = $accordion.find('.accordion-data');
+				
+				$accordionTrigger.first().addClass('open');
+				$accordionDatabox.first().show();
+				
+				$accordionTrigger.on('click',function (e) {
+					var $this = $(this);
+					var $accordionData = $this.next('.accordion-data');
+					if( $accordionData.is($accordionDatabox) &&  $accordionData.is(':visible') ){
+						$this.removeClass('open');
+						$accordionData.slideUp(400);
+						e.preventDefault();
+					} else {
+						$accordionTrigger.removeClass('open');
+						$this.addClass('open');
+						$accordionDatabox.slideUp(400);
+						$accordionData.slideDown(400);
+					}
+				});
+		});
+		
+		
+		/* Mobile menu click
+		---------------------------------------------------------------------*/
+		$(document).on('click',"#menu", function(){
+			$(this).toggleClass('menuopen');
+			$(this).next('ul').slideToggle('normal');
+			return false;
+		});
+
+
+		/* Header Sticky
+		---------------------------------------------------------------------*/
+		if($("#header").length) {
+			$(window).scroll(function() {
+				var headerHeight = $('#header').outerHeight() + 100;
+				if( $(this).scrollTop() > headerHeight ) {
+					$("#header").addClass("sticky");
+				} else {
+					$("#header").removeClass("sticky");
+				}
+			});
+			var header = document.querySelectorAll('#header');
+			Stickyfill.add(header);
+		}
+
+
+		
+		
+/*--------------------------------------------------------------------------------------------------------------------------------------*/		
+	});	
+
+/*All function need to define here for use strict mode
+----------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+	
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+})(jQuery, window, document);
