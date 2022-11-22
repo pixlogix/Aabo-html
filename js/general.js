2d0ce893d77676b2d98599312e4713c15fd946bb
Pixlogix <pm@pixlogix.com>
2022-11-22 14:23:37 +0530

    homepage complete desktop
    homepage complete desktop

--- js/general.js
+++ js/general.js
@@ -195,6 +195,35 @@
 		}
 
 
+		// jQuery counter up
+		if($(".counter").length) {
+			var counterUp = window.counterUp["default"]; // import counterUp from "counterup2"
+			var $counters = $(".counter");
+			/* Start counting, do this on DOM ready or with Waypoints. */
+			$counters.each(function (ignore, counter) {
+				var waypoint = new Waypoint( {
+					element: $(this),
+					handler: function() { 
+						counterUp(counter, {
+							duration: 3000,
+							delay: 50
+						}); 
+						this.destroy();
+					},
+					offset: 'bottom-in-view',
+				} );
+			});
+		}
+
+
+
+
+
+
+
+
+
+
 		/* Director-Swiper*/
 
 		var swiper = new Swiper(".directorSwiper", {
