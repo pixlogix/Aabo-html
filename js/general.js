ecdb2b562de567eb0386fe270c1c444400f09948
Pixlogix <pm@pixlogix.com>
2022-11-28 19:43:46 +0530

    we are hiring pop up form and construction companies page with responsive
    we are hiring pop up form and construction companies page with responsive

--- js/general.js
+++ js/general.js
@@ -286,6 +286,30 @@
 			return false;
 		});
 
+		// drop box js
+    if($(".drop_box").length) {
+			const dropArea = document.querySelector(".drop_box"),
+				button = dropArea.querySelector("button"),
+				dragText = dropArea.querySelector("header"),
+				input = dropArea.querySelector("input");
+			let file;
+			var filename;
+
+			button.onclick = () => {
+				input.click();
+			};
+
+			input.addEventListener("change", function (e) {
+				var fileName = e.target.files[0].name;
+				let filedata = `
+					<form action="" method="post">
+					<div class="form">
+					<button class="button btn-outline">Upload</button>
+					</div>
+					</form>`;
+				dropArea.innerHTML = filedata;
+			});
+	}
 
 
 
